af626502f19540e059fa74a4107d744cd43b4121
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-09-26 00:57:13 +0200

    init stack
    ^vue+tailwind^

---
+++ src/page/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-container">
+    <div class="flex-row">
+      <span class="gradient-css">This is a gradient written in CSS code</span>
+      <div class="image"></div>
+      <span class="css-description"
+        >Cascading Style Sheets (CSS) is the code that describes the font, the
+        background color and the disposition of elements on the page.. and many
+        more!</span
+      ><span class="text"></span>
+    </div>
+    <span class="outline-css">This is an outline written in CSS code</span>
+    <div class="flex-row-1">
+      <div class="image-2"></div>
+      <div class="arrow"></div>
+      <span class="svg-file">This is the SVG file<br /></span>
+      <div class="walk"></div>
+      <div class="image-3"></div>
+      <div class="arrow-4"></div>
+      <div class="arrow-5"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="javascript"></script>
+
+<style src="./index.css"></style>
